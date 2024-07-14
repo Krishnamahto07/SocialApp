621e0f54c50cfc0937f6074a27fbc18b8092f4dc
Krishna <krishnamahato84044@gmail.com>
2024-07-14 23:37:15 +0530

    end

--- server/index.js
+++ server/index.js
@@ -37,7 +37,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+	cors({
+		origin:"*",
+		credentials:true,
+	})
+)
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 const port = process.env.PORT || 6000;
